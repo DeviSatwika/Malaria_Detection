24eb926566eb81b70c11ae1d2f925c37f76bf124
Devi Satwika <104996625+DeviSatwika@users.noreply.github.com>
2024-10-25 14:54:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template, redirect, url_for, flash
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import os
+
+app = Flask(__name__)
+app.secret_key = "supersecretkey"
+model = load_model('malaria_model.h5')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = os.path.join('uploads', file.filename)
+        file.save(filename)
+
+        img = image.load_img(filename, target_size=(150, 150))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
+
+        prediction = model.predict(img_array)
+        os.remove(filename)
+
+        if prediction[0][0] > 0.5:
+            result = "Uninfected"
+        else:
+            result = "Infected"
+
+        return render_template('result.html', prediction=result)
+    else:
+        flash('Allowed file types are jpg, jpeg, png')
+        return redirect(request.url)
+
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
